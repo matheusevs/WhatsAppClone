7b4bd44501df72924675ed46b505a98adb20738b
MatheusEduardo <matheus.eduardo@versatecnologia.com.br>
2023-04-10 16:30:53 -0300

    Iniciando o projeto

---
+++ src/controller/WhatsAppController.js
@@ -0,0 +1,28 @@
+class WhatsAppController {
+    
+    constructor(){
+
+        this.elementsPrototype();
+        this.loadElements();
+
+    }
+
+    loadElements(){
+
+        this.el = {};
+        
+        document.querySelectorAll('[id]').forEach(element => {
+
+            this.el[Format.getCamelCase(element.id)] = element;
+
+        });
+
+    }
+
+    elementsPrototype(){
+
+        Element.prototype.hide
+
+    }
+
+}
