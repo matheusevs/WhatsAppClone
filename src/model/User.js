8698bb8d9d6d881cf68e4f0a0502308beacac08e
MatheusEduardo <matheus.eduardo@versatecnologia.com.br>
2023-07-27 11:58:46 -0300

    Adicionando model User para set, get, save e getRef do firebase - AULA 140

--- src/model/User.js
+++ src/model/User.js
@@ -1,8 +1,60 @@
-
-import { ClassEvent } from '../util/ClassEvent';
 import { Firebase } from './../util/Firebase';
+import { Model } from './Model';
 
-export class User extends ClassEvent{
+export class User extends Model {
+
+    constructor(id){
+
+        super();
+        if(id) this.getById(id);
+
+    }
+
+    get name(){
+        return this._data.name;
+    }
+
+    set name(value){
+        this._data.name = value;
+    }
+
+    get email(){
+        return this._data.email;
+    }
+
+    set email(value){
+        this._data.email = value;
+    }
+
+    get photo(){
+        return this._data.photo;
+    }
+
+    set photo(value){
+        this._data.photo = value;
+    }
+
+    getById(id){
+
+        return new Promise((s, f) => {
+
+            User.findByEmail(id).onSnapshot(doc => {
+
+                this.fromJSON(doc.data());
+
+                s(doc);
+
+            });
+
+        });
+
+    }
+
+    save(){
+
+        return User.findByEmail(this.email).set(this.toJSON());
+
+    }
 
     static getRef(){
 
@@ -12,6 +64,7 @@ export class User extends ClassEvent{
 
     static findByEmail(email){
 
+        return User.getRef().doc(email);
 
     }
 
