63fae650c93f2ca47425eee5e4aa2f387fb7d794
Matheus Eduardo <88885147+matheusevs@users.noreply.github.com>
2023-08-01 14:04:05 -0300

    Notificação desktop de novas mensagens - 161

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -14,11 +14,68 @@ export class WhatsAppController {
     
     constructor(){
 
+        this._active = true;
         this._firebase = new Firebase();
         this.initAuth();
         this.elementsPrototype();
         this.loadElements();
         this.initEvents();
+        this.checkNotifications();
+
+    }
+
+    checkNotifications(){
+        
+        if(typeof Notification === 'function'){
+
+            if(Notification.permission !== 'granted'){
+
+                this.el.alertNotificationPermission.show();
+
+            } else {
+
+                this.el.alertNotificationPermission.hide();
+
+            }
+
+            this.el.alertNotificationPermission.on('click', e => {
+
+                Notification.requestPermission(permission => {
+
+                    if(permission === 'granted'){
+
+                        this.el.alertNotificationPermission.hide();
+
+                    }
+
+                });
+
+            })
+
+        }
+    
+    }
+
+    notification(data){
+
+        if(Notification.permission === 'granted' && !this._active){
+
+            let n = new Notification(this._contactActive.name, {
+                icon: this._contactActive.photo,
+                body: data.content
+            });
+
+            let sound = new Audio('./audio/alert.mp3');
+            sound.currentTime = 0;
+            sound.play();
+
+            setTimeout(() => {
+
+                if(n) n.close();
+
+            }, 3000);
+
+        }
 
     }
 
@@ -195,6 +252,7 @@ export class WhatsAppController {
             display: 'flex'
         });
 
+        this._messagesReceived = [];
 
         Message.getRef(this._contactActive.chatId).orderBy('timeStamp')
             .onSnapshot(docs => {
@@ -213,6 +271,14 @@ export class WhatsAppController {
                     message.fromJSON(data);
 
                     let me = (data.from === this._user.email);
+
+                    if(!me && this._messagesReceived.filter(id => {return (id === data.id)}).length === 0){
+
+                        this.notification(data);
+                        this._messagesReceived.push(data.id);
+
+                    }
+
                     let view = message.getViewElement(me);
 
                     if(!this.el.panelMessagesContainer.querySelector(`#_${data.id}`)){
@@ -374,6 +440,18 @@ export class WhatsAppController {
     }
 
     initEvents(){
+
+        window.addEventListener('focus', e => {
+
+            this._active = true;
+
+        });
+
+        window.addEventListener('blur', e => {
+
+            this._active = false;
+
+        });
 
         this.el.myPhoto.on('click', e => {
 
